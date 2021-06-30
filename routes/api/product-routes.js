3da9f95e364788939666e0a9a98c5e1d2ff23604
Ant <alopez0218.al@gmail.com>
2021-06-30 03:12:45 -0700

    (Walkthrough + ReadMe)

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -47,8 +47,11 @@ router.post('/', (req, res) => {
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
-      if (req.body.tagIds.length) {
-        const productTagIdArr = req.body.tagIds.map((tag_id) => {
+      if (!product.tagIds) {
+        res.status(200).json(product);
+        return;
+      } else {
+        const productTagIdArr = product.tagIds.map((tag_id) => {
           return {
             product_id: product.id,
             tag_id,
@@ -56,8 +59,6 @@ router.post('/', (req, res) => {
         });
         return ProductTag.bulkCreate(productTagIdArr);
       }
-      // if no product tags, just respond
-      res.status(200).json(product);
     })
     .then((productTagIds) => res.status(200).json(productTagIds))
     .catch((err) => {
